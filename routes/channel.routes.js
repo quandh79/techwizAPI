5a235f66edd6a4a0809137f0c872e265c90603d8
hunghh90 <hungbkvn9x1@gmail.com>
2023-08-10 15:45:47 +0700

    hung

--- routes/channel.routes.js
+++ routes/channel.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 //const router = express.Router();
 const router = express.Router();
-const channelController = require("./../controllers/ChannelController");
+const channelController = require("../controllers/channelController");
 
 router.post("/create", channelController.createChannel);
 router.post("/get", channelController.getChannel);
--- routes/regservice.routes.js
+++ routes/regservice.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 //const router = express.Router();
 const router = express.Router();
-const regService = require("./../controllers/RegServiceController");
+const regService = require("../controllers/regServiceController");
 
 router.post("/subcribe", regService.registerProviderService);
 router.post("/isactive", regService.isActive);
