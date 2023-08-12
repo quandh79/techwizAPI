5fff6cce73c97068a972acefac96a98cc122b314
hunghh90 <hungbkvn9x1@gmail.com>
2023-08-12 10:31:26 +0700

    update feedbackRoute

--- routes/feedback.routes.js
+++ routes/feedback.routes.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const feedback = require("../controllers/feedbackController");
 const { protect } = require("../controllers/authController");
 
-router.post("/create", protect, feedback.create);
+router.post("/create", protect, feedback.createFeedback);
 router.post("/delete", protect, feedback.delete);
 router.post("/update", protect, feedback.update);
-router.get("/get", favorites.get);
-router.get("/getactive", favorites.getActive);
-router.get("/active", favorites.Active);
+router.get("/get", feedback.get);
+router.get("/getactive", feedback.getActive);
+router.get("/active", feedback.Active);
 
 module.exports = router;
